6a793976bf278e34847eb2de445091dee9239a37
2200090172 <2200090172@kluniversity.in>
2024-12-08 06:28:56 +0530

    Allowed cors

--- src/main/java/com/klef/jfsd/springboot/webConfig.java
+++ src/main/java/com/klef/jfsd/springboot/webConfig.java
@@ -13,7 +13,7 @@ public class webConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) 
     {
     	registry.addMapping("/**") // Allow CORS 
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://agriconnect-backend-deploy-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
